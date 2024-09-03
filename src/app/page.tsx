b1cb43c59085841e3eaa1337c10389e91787a591
tallescrhistiandev@gmail.com <tallescrhistiandev@gmail.com>
2024-09-02 21:11:01 -0300

    altera keys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import CardProduct from "@/Components/CardProduct/CardProduc";
 
 export default function Home() {
 
- const stringList = [<Image key={1}  className="w-full"  src={ImageLarga} alt="Test"/>, <Image key={2} className="w-full" src={ImageLarga} alt="Test"/>]
+ const stringList = [<Image key={"1"}  className="w-full"  src={ImageLarga} alt="Test"/>, <Image key={2} className="w-full" src={ImageLarga} alt="Test"/>]
 
   return (
     <main className="bg-primary-black">
